1191c4e8f18d7820f4ca96efab852be815ebb11f
shoren <65180338+shoren@users.noreply.github.com>
2022-09-17 18:34:47 -0600

    Got the dynamic link working for week1 assignment.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-This is the main for js.
+window.onload = function createList(){
+
+    //add new items to the array here!
+    let myArrayOfLinks =[
+        {"url": "https://www.google.com/","label":"This links to google!"},
+        {"url": "Url Goes here","label":"Label Goes here"},
+        {"url": "Url Goes here","label":"Label Goes here"}
+    ]
+
+
+    for(var i = 0; i < myArrayOfLinks.length; i++){
+        //create anchor tag
+        let articleElement = document.createElement('a');
+        document.getElementById('myOL').append(articleElement);
+        
+
+        // get the link
+        var myArray = myArrayOfLinks[i].label;
+        let hrefValue = myArrayOfLinks[i].url;
+        articleElement.href = hrefValue;
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(myArray));
+        articleElement.appendChild(li);
+    }
+}
